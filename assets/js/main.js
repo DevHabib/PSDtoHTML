5b169d75fadb601c227c45eac06c8dff4df29807
Md Habibur Rahman <habib@yotech.ltd>
2020-03-11 18:54:17 +0600

    Header to quote section almost completed, but team section has some problem which need to resolve later

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 jQuery(document).ready(function ($) {
-  $(".counter").counterUp({});
+
 });
 
-$('.grid').isotope({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  masonry: {
-    columnWidth: '.grid-sizer'
-  }
-});
+
+
+(function ($) {
+  "use strict";
+
+  // Jquery clickable functions starts
+  jQuery(document).ready(function ($) {
+    $(".counter").counterUp({});
+    $('.grid').isotope({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      masonry: {
+        columnWidth: '.grid-sizer'
+      }
+    });
+  });
+
+
+  jQuery(window).load(function () {
+    /* Sticky Header */
+    $(window).on('scroll', function () {
+      if ($(this).scrollTop() > 100) {
+        $('header').addClass("sticky");
+      } else {
+        $('header').removeClass("sticky");
+      }
+    });
+  });
+}(jQuery));
