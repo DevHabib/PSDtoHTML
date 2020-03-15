4dc0b828424819be46b4ec30b20a9c15325ac6fa
Md Habibur Rahman <habib@yotech.ltd>
2020-03-15 18:58:39 +0600

    Issues Found in internet explorerand need to fix the font size and spacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,10 @@ jQuery(document).ready(function ($) {
     });
   });
 }(jQuery));
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
